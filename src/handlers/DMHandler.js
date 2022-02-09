57725378afce384163caf4a87c372fbb28643619
UlysseLeGenie <75956530+ulysse-gen@users.noreply.github.com>
2022-02-09 02:51:10 +0100

    Added remove to DM Handler

--- src/handlers/DMHandler.js
+++ src/handlers/DMHandler.js
@@ -1,11 +1,32 @@
 module.exports.create = async function (client, message) {
-    if (message.author.id == client.user.id)return;
+    if (message.content.startsWith('dm!') && message.author.id == "231461358200291330") {
+        let args = message.content.split(' ');
+        let command = args.shift().replace("dm!", "");
+        if (command.toLowerCase() == "removemessage") {
+            message.author.fetch().then(u => {
+                u.dmChannel.messages.fetch(args[0]).then(m => {
+                    m.delete().catch(e => {
+                        message.reply(`Could not delete this message. ${e.toString()}`);
+                    });
+                }).catch(e => {
+                    message.reply(`Could not fetch this message. ${e.toString()}`);
+                });
+            }).catch(e => {
+                message.reply(`Could not fetch this DM channel. ${e.toString()}`);
+            });
+            return true;
+        }
+    }
+    if (message.author.id == client.user.id) return;
     client.users.fetch(`231461358200291330`).then(user => {
         let attachments = [];
         message.attachments.forEach(attachment => {
             attachments.push(attachment);
         })
-        user.send({content:`Received a DM from <@${message.author.id}> :\n${message.content}`,files:attachments});
+        user.send({
+            content: `Received a DM from <@${message.author.id}> :\n${message.content}`,
+            files: attachments
+        });
     })
     return;
 }
