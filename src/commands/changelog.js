11b4b3d6d4a477d7f82a141046b7cad44ced2583
UlysseLeGenie <75956530+ulysse-gen@users.noreply.github.com>
2022-03-14 02:21:21 +0100

    Fixed changelog lol

--- src/commands/changelog.js
+++ src/commands/changelog.js
@@ -16,9 +16,9 @@ module.exports = {
     category: `informations`,
     async exec(client, message, args, guild = undefined) {
         let description = `- Modlogs now show unbans infos.`
-        description = `\n- Modlogs now show unmutes infos.`
-        description = `\n- Replying to a user with \`ef\` command now reply to that message with the embedfail gif.`
-        description = `\n- Russian Roulette now auto pin & unpin the main message if the start timer is above 150 seconds.`
+        description += `\n- Modlogs now show unmutes infos.`
+        description += `\n- Replying to a user with \`ef\` command now reply to that message with the embedfail gif.`
+        description += `\n- Russian Roulette now auto pin & unpin the main message if the start timer is above 150 seconds.`
         return utils.sendMain(message, guild, `Changelog v${package.version}`, `${description}`, [], true);
     }
 }
