4e8119c8d4d1de33011cbb74b211e55f9d0e14de
UlysseLeGenie <75956530+ulysse-gen@users.noreply.github.com>
2022-03-20 02:25:24 +0100

    Changelog & package.json updates

--- src/commands/changelog.js
+++ src/commands/changelog.js
@@ -19,8 +19,7 @@ module.exports = {
     permission: `commands.changelog`,
     category: `informations`,
     async exec(client, message, args, guild = undefined) {
-        description = `\n- New \`sticky\` command with \`pin\` alias. Kinda like a note but appear on top of every others player punishments on \`modlogs\`.`
-        description += `\n- New \`stickyhistory\` command with \`sh\` alias. List the history of all the sticky notes that a user ever had.`
+        let description = `Internal fixes and improvements.`
         return utils.sendMain(message, guild, `Changelog v${packageJson.version}`, `${description}`, [], true);
     }
 }
