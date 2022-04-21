f9897027f21bbef9be1e5e14b424ca389b242824
UlysseLeGenie <75956530+ulysse-gen@users.noreply.github.com>
2022-04-21 23:42:48 +0200

    enabled error catching

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ var client = new Client({
     intents: intents
 });
 var rest = undefined;
-const errorCatching = false;
+const errorCatching = true;
 
 //CommandManagers & PermissionsManagers
 var globalCommands = new commandsManager(client);
