bf09a5762bf05b3ec927ee87bd62060838cb5db2
UlysseLeGenie <75956530+ulysse-gen@users.noreply.github.com>
2022-07-09 16:11:17 +0200

    Updated SQL structore

--- tobybot-structure.sql
+++ tobybot-structure.sql
@@ -1,5 +1,12 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
 
 CREATE TABLE `guilds` (
   `numId` int(11) NOT NULL,
@@ -7,7 +14,16 @@ CREATE TABLE `guilds` (
   `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `locale` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'en-US',
   `configuration` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`configuration`)),
-  `permissions` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`permissions`))
+  `permissions` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`permissions`)),
+  `lastUpdated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE `logs` (
+  `numId` int(11) NOT NULL,
+  `type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'undefined',
+  `context` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '{}',
+  `content` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '{}' CHECK (json_valid(`content`)),
+  `timestamp` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE `moderation` (
@@ -22,12 +38,13 @@ CREATE TABLE `moderation` (
   `updateReason` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `logs` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '[]' CHECK (json_valid(`logs`)),
   `expires` timestamp NOT NULL DEFAULT current_timestamp(),
-  `updateTimestamp` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updateTimestamp` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `timestamp` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE `tobybot` (
   `numId` int(11) NOT NULL,
+  `locale` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `configuration` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`configuration`)),
   `permissions` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`permissions`))
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -35,13 +52,16 @@ CREATE TABLE `tobybot` (
 CREATE TABLE `users` (
   `numId` int(11) NOT NULL,
   `id` varchar(18) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `configuration` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`configuration`)),
-  `permissionLevel` int(11) NOT NULL DEFAULT 1
+  `permissionLevel` int(11) NOT NULL DEFAULT 10
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 
 ALTER TABLE `guilds`
+  ADD PRIMARY KEY (`numId`);
+
+ALTER TABLE `logs`
   ADD PRIMARY KEY (`numId`);
 
 ALTER TABLE `moderation`
@@ -55,6 +75,9 @@ ALTER TABLE `users`
 
 
 ALTER TABLE `guilds`
+  MODIFY `numId` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `logs`
   MODIFY `numId` int(11) NOT NULL AUTO_INCREMENT;
 
 ALTER TABLE `moderation`
@@ -66,3 +89,7 @@ ALTER TABLE `tobybot`
 ALTER TABLE `users`
   MODIFY `numId` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
